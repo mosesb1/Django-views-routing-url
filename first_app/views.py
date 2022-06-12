368feb01ad35bfe5c3d820fccd084838e7361778
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-12 09:24:54 -0400

    added url reverse function to redirect

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 articles = {
@@ -9,9 +10,18 @@ articles = {
 }
 
 def news_view(request,topic):
-    return HttpResponse(articles[topic])
+    try:
+        return HttpResponse(articles[topic]) if topic != '0' else HttpResponseRedirect('/first_app/sports')
+    except:
+        raise Http404("404 GENERIC ERROR")
 
 def add_view(request,num1,num2):
     add_result = num1 + num2
     result = f"{num1} + {num2} = {add_result}"
     return HttpResponse(result)
+
+def num_page_view(request,num_page):
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+
+    return HttpResponseRedirect(reverse('topic-page',args=[topic]))
