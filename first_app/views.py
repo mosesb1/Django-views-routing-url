c27ab0c71b8bc40b60386001ef3cf9b298c31e6b
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-12 09:40:18 -0400

    connect template to view

--- first_app/views.py
+++ first_app/views.py
@@ -3,25 +3,5 @@ from django.http.response import HttpResponse, HttpResponseNotFound, Http404, Ht
 from django.urls import reverse
 # Create your views here.
 
-articles = {
-    'sports': 'Sports Page',
-    'finance': 'Finance Page',
-    'politics': 'Politics Page'
-}
-
-def news_view(request,topic):
-    try:
-        return HttpResponse(articles[topic]) if topic != '0' else HttpResponseRedirect('/first_app/sports')
-    except:
-        raise Http404("404 GENERIC ERROR")
-
-def add_view(request,num1,num2):
-    add_result = num1 + num2
-    result = f"{num1} + {num2} = {add_result}"
-    return HttpResponse(result)
-
-def num_page_view(request,num_page):
-    topics_list = list(articles.keys())
-    topic = topics_list[num_page]
-
-    return HttpResponseRedirect(reverse('topic-page',args=[topic]))
+def simple_view(request):
+    return render(request,'first_app/example.html')
